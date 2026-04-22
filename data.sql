cdbf23b05b1c74d6b3dd8de990ab279b236f289e
ProfessorCurry02 <bharath.shanmugasundaram@gmail.com>
2026-04-22 14:21:29 -0500

    Add employee records to the EMPLOYEE table
    Inserted multiple employee records into the EMPLOYEE table.

---
+++ data.sql
@@ -0,0 +1,11 @@
+USE [InterviewPracticeDB]
+GO
+
+INSERT INTO EMPLOYEE (Emp_Email, Country, City, Emp_Age, Salary, Emp_Fname, Emp_Lname, Emp_DOB, Emp_Join_Date, Department)
+VALUES
+('john.doe@email.com', 'USA', 'Dallas', 30, 60000, 'John', 'Doe', '1994-05-10', '2022-06-01', 'IT'),
+('jane.smith@email.com', 'USA', 'Plano', 28, 55000, 'Jane', 'Smith', '1996-08-15', '2021-07-15', 'HR'),
+('mike.brown@email.com', 'USA', 'Austin', 35, 70000, 'Mike', 'Brown', '1989-03-22', '2020-01-10', 'Finance');
+GO
+
+
